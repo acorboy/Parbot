72fb5289fd1027a7d526b0c833197499df9578c7
Drew <acorboy@gmail.com>
2018-06-25 18:19:50 -0500

    Initial draft with most logic stubbed out

---
+++ gen_contact_name.py
@@ -0,0 +1,11 @@
+# Generate the contact name
+
+# ISSUE: Should this return name and email address?
+
+# Add a list of the first names of everyone on the project
+# Add a list of the last names of everyone on the project
+# Randomly combine first and last names for name and email
+
+def gen_contact_name():
+    name = 'Oscar D. Grouch'
+    return name
