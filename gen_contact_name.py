8529dcdb4546bfef2a7773306a32988839a4b161
Drew <acorboy@gmail.com>
2018-06-27 10:53:38 -0500

    Deprecated.  Prabhat's Faker code will replace this.

--- gen_contact_name.py
+++ gen_contact_name.py
@@ -1,10 +1,4 @@
-# Generate the contact name
-
-# ISSUE: Should this return name and email address?
-
-# Add a list of the first names of everyone on the project
-# Add a list of the last names of everyone on the project
-# Randomly combine first and last names for name and email
+# Deprecated. Replaced by Prabhat's Faker code
 
 def gen_contact_name():
     name = 'Oscar D. Grouch'
