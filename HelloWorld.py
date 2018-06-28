f506f5dfd9f21e53bd51a4609afb5d468ad3d8d5
Drew <acorboy@gmail.com>
2018-06-28 11:16:24 -0500

    Virtual environment

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+import random
+#for i in range(1,10):
+ #   print("Random: ", random.randint(1,100))
+##comment
+
+from datetime import date
+today = str(date.today())
+print(today)   # '2017-12-26'
