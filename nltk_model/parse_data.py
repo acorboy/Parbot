cd08284cf99ef88e94a302706362bf528a645a61
cakoch10 <cakoch10@gmail.com>
2018-06-28 18:53:07 -0500

    fixed webapp

--- nltk_model/parse_data.py
+++ nltk_model/parse_data.py
@@ -3,10 +3,7 @@ from nltk.tokenize import word_tokenize
 
 st = StanfordNERTagger('/Users/temp_account/Downloads/stanford-ner-2018-02-27/classifiers/english.muc.7class.distsim.crf.ser.gz','/Users/temp_account/Downloads/stanford-ner-2018-02-27/stanford-ner.jar', encoding='utf-8')
 
-text = "Perez-Turner Ryan, Simpson and Escobar Bartlett, James and Robinson,423352301c8142bc13ea4344115013b5,15-7380349,27.523780:83.345585,8288140737,68297 Scott Orchard,South Derrick,NE,31056,United States of America,203-220-8928,Susan Bender,Engineer, biomedical,1-552-632-8506,evanscody@oneill.info"
-
-tokenized_text = word_tokenize(text)
-classified_text = st.tag(tokenized_text)
-
-for i in classified_text:
-    print(i)
+def parse_data(str_data):
+    tokenized_text = word_tokenize(str_data)
+    classified_text = st.tag(tokenized_text)
+    return classified_text
--- webapp/app.py
+++ webapp/app.py
@@ -8,10 +8,12 @@ def index_view():
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    print("RECEIVED POST")
     # get file
     str_data = request.data
-    return parse_data(str_data)
+    # return parse_data(str_data)
+    return "received data!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0',port=1111)
